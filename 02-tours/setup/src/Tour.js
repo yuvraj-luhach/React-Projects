63c64f4e7e7c8b43117925c7eda6c74fe23b9856
Yuvraj <yuvraj.gwps@gmail.com>
2022-10-18 22:04:48 +0530

    done

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ tour, remTour }) => {
+  const { id, name, info, image, price } = tour;
+
+  const [showAll, setShowAll] = useState(false);
+  const showMore = () => setShowAll(true);
+  const showLess = () => setShowAll(false);
+  const toShow = info.substring(0, 200) + "...";
+
+  return (
+    <article className="single-tour">
+      <img src={image} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        {/* We show the extended text and a link to reduce it */}
+        {showAll ? (
+          <p>
+            {info}
+            <button onClick={showLess}>Show less</button>
+          </p>
+        ) : (
+          <p>
+            {toShow}
+            <button onClick={showMore}>Read more</button>
+          </p>
+        )}
+        <button className="delete-btn" onClick={() => remTour(id)}>
+          Not Interested
+        </button>
+      </footer>
+    </article>
+  );
 };
 
 export default Tour;
--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,20 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ tours, remTour }) => {
+  return (
+    <main>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour tour={tour} key={tour.id} remTour={remTour} />;
+          // return <Tour key={tour.id} {...tour} />;
+        })}
+      </div>
+    </main>
+  );
 };
 
 export default Tours;
