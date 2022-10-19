a45378d2ab2b3c0d094aead07b37790735148b83
Yuvraj <yuvraj.gwps@gmail.com>
2022-10-19 15:49:10 +0530

    done

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import Question from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and Answers About Login</h3>
+        <section className="info">
+          {data.map((question) => {
+            return <Question key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ id, title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+
+  const toggle = () => {
+    setShowInfo((showInfo) => {
+      return !showInfo;
+    });
+  };
+
+  return (
+    <div className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={toggle}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </div>
+  );
 };
 
 export default Question;
