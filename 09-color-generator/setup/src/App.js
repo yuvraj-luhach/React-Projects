19262e33172232d7650a71d2ff71f09f5ab4ff31
Yuvraj <yuvraj.gwps@gmail.com>
2022-10-20 14:16:34 +0530

    done

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -1,10 +1,55 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#1ff").all(10));
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    try {
+      let colors = new Values(color).all(5);
+      console.log(colors);
+      setList(colors);
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
+  };
+
+  const changeColor = () => {};
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form action="" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name=""
+            id=""
+            value={color}
+            onChange={(event) => setColor(event.target.value)}
+            placeholder="#1ff"
+            // className={error && "error"}
+            className={`${error ? "error" : null}`}
+          />
+          <button type="submit" className="btn">
+            generate
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          // console.log(color);
+          return <SingleColor key={index} {...color} index={index} />;
+        })}
+        <h4></h4>
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
