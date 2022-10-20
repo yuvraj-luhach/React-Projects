14678f02e55a560e3c359f2b5a59c20c0e5bb07c
Yuvraj <yuvraj.gwps@gmail.com>
2022-10-20 10:08:47 +0530

    done

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,45 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > data.length) {
+      amount = data.length;
+    }
+    setText(data.slice(0, amount));
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>TIRED OF BORING LOREM IPSUM?</h3>
+      <form action="" className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(event) => setCount(event.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+
+      <article className="lorem-text">
+        {text.map((paragraph, index) => {
+          return <p key={index}>{paragraph}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
